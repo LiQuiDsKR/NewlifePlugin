cd1f33adc5cce9aab4cf062ecb67df619c57e741
Lee Jae Hyeong <littleplayer777@gmail.com>
2023-05-25 03:37:37 +0900

    프로토타입 완성
    미션 Type 객체 리스트로 수정
    /team1, /team2 로 팀 지정 기능 추가
    /check 로 미션 클리어 여부 감지 기능 추가
    /teaminfo 는 디버그용, 팀 소속 로그 띄움
    /team rename <팀이름> 으로 변경 가능
    /reinforce 로 랜덤강화 기능 추가 (책 강화는 아직 냅뒀는데 삭제 예정)
    /mission 으로 미션 현황 UI 확인 가능
    겉날개 착용 불가 규칙 추가
    엔더드래곤이 죽으면 5분 뒤에 모두 오버월드로 강제이동 추가
    임시 미션 '금 수집가', '금 부자', '금 억만장자' 추가

---
+++ src/main/java/com/newlifeplugin/NewlifePlugin.java
@@ -0,0 +1,303 @@
+package com.newlifeplugin;
+
+import com.destroystokyo.paper.MaterialTags;
+import io.papermc.paper.event.player.PlayerInventorySlotChangeEvent;
+import org.bukkit.*;
+import org.bukkit.entity.Item;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public final class NewlifePlugin extends JavaPlugin implements Listener {
+    private HashMap<String, String> teams;
+    private Random random;
+
+    Inventory missionInventory = Bukkit.createInventory(null, 27, "Missions");
+
+    public class Mission {
+        String missinName;
+        String difficulty;
+        String missionDetail;
+        Material displayMaterial;
+        String clearedTeam;
+    }
+    public List<Mission> missions = new ArrayList<Mission>() {
+
+    };
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+        teams = new HashMap<>();
+        random = new Random();
+
+        // Set initial team names
+        teams.forEach((key, value) -> getConfig().set("teams." + key, value));
+        saveConfig();
+
+        // Modify game rules
+        World world = Bukkit.getWorlds().get(0); // Assuming only one world
+        world.setGameRule(GameRule.REDUCED_DEBUG_INFO, true);
+        world.getWorldBorder().setCenter(0, 0);
+        world.getWorldBorder().setSize(2000);
+
+        missions.add (createMission("금 수집가", "난이도 하", "금 블럭 10개를 모으세요.", Material.GOLD_NUGGET, "미달성"));
+        missions.add (createMission("금 부자", "난이도 중", "금 블럭 32개를 모으세요.", Material.GOLD_INGOT, "미달성"));
+        missions.add (createMission("금 억만장자", "난이도 상", "금 블럭 64개를 모으세요.", Material.GOLD_BLOCK, "미달성"));
+    }
+
+    private Mission createMission(String missionName, String difficulty, String missionDetail, Material displayMaterial, String clearedTeam) {
+        Mission mission = new Mission();
+        mission.missinName = missionName;
+        mission.difficulty = difficulty;
+        mission.missionDetail = missionDetail;
+        mission.displayMaterial = displayMaterial;
+        mission.clearedTeam = clearedTeam;
+
+        return mission;
+    }
+
+    private ItemStack createMissionItem(String missionName, String difficulty, String missionDetail, Material displayMaterial, String clearedTeam) {
+        ItemStack item = new ItemStack(displayMaterial);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName("§r" + ChatColor.WHITE + missionName);
+        List<String> lores = new ArrayList<String>() {
+        };
+        if (difficulty == "난이도 하") {
+            lores.add("§r" + ChatColor.GREEN + difficulty);
+        } else if (difficulty == "난이도 중") {
+            lores.add("§r" + ChatColor.GOLD + difficulty);
+        } else if (difficulty == "난이도 상") {
+            lores.add("§r" + ChatColor.RED + difficulty);
+        }
+            lores.add("§r" + ChatColor.GRAY + missionDetail);
+        lores.add("§r" + ChatColor.BLUE + clearedTeam);
+
+        meta.setLore(lores);
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
+    @EventHandler
+    public void onPlayerInventorySlotChanged(PlayerInventorySlotChangeEvent event) {
+        if (event.getPlayer().getInventory().getChestplate().getType() == Material.ELYTRA){
+            event.getPlayer().getInventory().setChestplate(new ItemStack(Material.AIR));
+            event.getPlayer().sendMessage("You can't equip Elytra!");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        String command = event.getMessage().toLowerCase();
+
+
+
+        if (command.equals("/team1")) {
+            event.setCancelled(true);
+            String playerName = event.getPlayer().getName();
+            teams.put(playerName, "team1");
+        } else if (command.equals("/team2")) {
+            event.setCancelled(true);
+            String playerName = event.getPlayer().getName();
+            teams.put(playerName, "team2");
+        } else if (command.equals("/check")) {
+            event.setCancelled(true);
+            checkGoldRich1(event.getPlayer());
+            checkGoldRich2(event.getPlayer());
+            checkGoldRich3(event.getPlayer());
+        } else if (command.equals("/teaminfo")) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(teams.keySet().toString());
+            event.getPlayer().sendMessage(teams.values().toString());
+
+        } else if (command.startsWith("/team rename")) {
+            event.setCancelled(true);
+            Player player = event.getPlayer();
+            String[] parts = command.split(" ");
+            if (parts.length == 3 && parts[2].length() > 0) {
+                String newTeamName = parts[2];
+                for (String playerName : teams.keySet()) {
+                    if (teams.get(playerName) == teams.get(player.getName())) {
+                        teams.replace(playerName, newTeamName);
+                    }
+                }
+            }
+        } else if (command.equals("/reinforce")) {
+            event.setCancelled(true);
+            Player player = event.getPlayer();
+            ItemStack item = player.getInventory().getItemInMainHand();
+
+            if (item != null && item.getType() != Material.AIR) {
+                if (player.getLevel() >= 1) {
+                    enchantItemRandomly(item);
+                    player.setLevel(player.getLevel() - 1);
+                    player.sendMessage("Your item has been enchanted!");
+                } else {
+                    player.sendMessage("You don't have enough levels to perform an enchantment.");
+                }
+            } else {
+                player.sendMessage("You must be holding an item to enchant it.");
+            }
+        } else if (command.equals("/mission")) {
+            event.setCancelled(true);
+            openMissionUI(event.getPlayer());
+        }
+    }
+
+    private void checkGoldRich1(Player player) {
+
+        int requiredAmount = 10;
+        ItemStack targetItem = new ItemStack(Material.GOLD_BLOCK, requiredAmount);
+
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && item.isSimilar(targetItem) && item.getAmount() >= requiredAmount) {
+                for (Mission m :missions) {
+                    if (m.missinName == "금 수집가" && m.clearedTeam == "미달성") {
+                        m.clearedTeam = teams.get(player.getName());
+                    }
+                }
+            }
+        }
+    }
+    private void checkGoldRich2(Player player) {
+
+        int requiredAmount = 32;
+        ItemStack targetItem = new ItemStack(Material.GOLD_BLOCK, requiredAmount);
+
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && item.isSimilar(targetItem) && item.getAmount() >= requiredAmount) {
+                for (Mission m :missions) {
+                    if (m.missinName == "금 부자" && m.clearedTeam == "미달성") {
+                        m.clearedTeam = teams.get(player.getName());
+                    }
+                }
+            }
+        }
+    }
+    private void checkGoldRich3(Player player) {
+
+        int requiredAmount = 64;
+        ItemStack targetItem = new ItemStack(Material.GOLD_BLOCK, requiredAmount);
+
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && item.isSimilar(targetItem) && item.getAmount() >= requiredAmount) {
+                for (Mission m :missions) {
+                    if (m.missinName == "금 억만장자" && m.clearedTeam == "미달성") {
+                        m.clearedTeam = teams.get(player.getName());
+                    }
+                }
+            }
+        }
+    }
+    private void openMissionUI(Player player) {
+        updateMission();
+        player.openInventory(missionInventory);
+    }
+    private void updateMission() {
+
+        Inventory inventory = missionInventory;
+        inventory.clear();
+        for (Mission m : missions) {
+            missionInventory.addItem(createMissionItem(m.missinName, m.difficulty, m.missionDetail, m.displayMaterial, m.clearedTeam));
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getView().getTitle().equalsIgnoreCase("Missions")) {
+            event.setCancelled(true);
+        }
+    }
+
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (event.getEntity() instanceof EnderDragon) {
+            Bukkit.broadcastMessage("The Ender Dragon has been defeated!");
+
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    teleportFromEnd();
+                }
+            }.runTaskLater(this, 5 * 60 * 20); // 5 minutes * 60 seconds * 20 ticks
+        }
+    }
+
+
+
+    private void enchantItemRandomly(ItemStack item) {
+
+        if (item.getType() == Material.ENCHANTED_BOOK) {
+            // If item is an enchanted book, add enchantments to the book
+            item.addUnsafeEnchantment(getRandomEnchantment(), random.nextInt(5) + 1);
+        } else {
+            // If item is not an enchanted book, directly enchant the item
+            for (Enchantment i:item.getEnchantments().keySet()) {
+                item.removeEnchantment(i);
+            }
+            for (int i = 0; i < random.nextInt(5) + 1; i++) {
+                setEnchantment(item);
+            }
+        }
+    }
+
+    void setEnchantment(ItemStack item) {
+        try {
+            Enchantment temp;
+            temp = getRandomEnchantment();
+            item.addEnchantment(temp, random.nextInt(temp.getMaxLevel()));
+        } catch (IllegalArgumentException e) {
+            setEnchantment(item);
+        }
+    }
+
+
+    private Enchantment getRandomEnchantment() {
+        Enchantment[] enchantments = Enchantment.values();
+        return enchantments[random.nextInt(enchantments.length)];
+    }
+
+    private void teleportFromEnd() {
+        World endWorld = Bukkit.getWorlds().get(1); // Assuming the End world is the second world loaded
+        List<Player> playersInEnd = endWorld.getPlayers();
+
+        if (!playersInEnd.isEmpty()) {
+            for (Player player : playersInEnd) {
+                player.setVelocity(new Vector(0, 0, 0));
+                player.teleport(player.getBedSpawnLocation());
+                player.sendMessage("You have been teleported back to the Overworld.");
+            }
+        }
+    }
+}
+
